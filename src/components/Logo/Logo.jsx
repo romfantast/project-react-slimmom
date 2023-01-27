b411ae72e667a5e94d1106fbc844612289b12d1f
romfantast <romfantast@gmail.com>
2023-01-28 00:22:08 +0200

    Update Logo.jsx

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import logo from '../../images/logo.png';
 import css from './Logo.module.css';
+import { NavLink } from 'react-router-dom';
 
 function Logo() {
   return (
     <div className={css.logoWrapper}>
-      <img className={css.logo} src={logo} alt="" />
-      <span className={css.logoText}>
-        Slim<span>Mom</span>
-      </span>
+      <NavLink to="/">
+        <img className={css.logo} src={logo} alt="" />
+        <span className={css.logoText}>
+          Slim<span>Mom</span>
+        </span>
+      </NavLink>
     </div>
   );
 }
